59450e12da63ac1d274c510e6930109425ed09f8
Nicolo Vinci <vincinicolo98@gmail.com>
2020-02-01 21:46:05 +0100

    aggiornamento veloce

--- docker/umidificatore/build/umidificatore.js
+++ docker/umidificatore/build/umidificatore.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Umidificatore = /** @class */ (function () {
     function Umidificatore(_delta) {
         this.delta = 0;
+        this.enable = false; //simula attivazione
         this.currentValue = 0;
         this.target = 0;
         this.delta = _delta;
@@ -10,13 +11,16 @@ var Umidificatore = /** @class */ (function () {
     Umidificatore.prototype.active = function () {
         if (this.currentValue < this.target) {
             this.currentValue = +this.delta;
+            this.enable = true;
             return "plus";
         }
         else if (this.currentValue > this.target) {
             this.currentValue = -this.delta;
+            this.enable = true;
             return "minus";
         }
         else {
+            this.enable = false;
             return "disabled";
         }
     };
--- docker/umidificatore/src/umidificatore.ts
+++ docker/umidificatore/src/umidificatore.ts
@@ -1,6 +1,7 @@
 export class Umidificatore {
 
     private delta: number = 0;
+    private enable: boolean = false; //simula attivazione
 
     currentValue: number = 0;
     target: number = 0;
@@ -12,11 +13,14 @@ export class Umidificatore {
     active(): string {
         if(this.currentValue < this.target) {
             this.currentValue =+ this.delta;
+            this.enable = true;
             return "plus";
         } else if (this.currentValue > this.target) {
             this.currentValue =- this.delta;
+            this.enable = true;
             return "minus";
         } else {
+            this.enable = false;
             return "disabled";
         }
     }
