1aba2b7223b3b8c2a1e43b777d36a2057fd27e9f
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2020-02-07 09:57:39 +0100

    Update ip adresses

--- topology.py
+++ topology.py
@@ -8,6 +8,7 @@ from mininet.link import TCLink
 
 CPU_SETS = "0"
 
+
 def broker():
     info("Add host brokermqtt\n")
     brokermqtt = net.addDockerHost(
@@ -20,12 +21,13 @@ def broker():
     info("Add link\n")
     net.addLink(switch, brokermqtt, bw=10, delay="10ms")
 
+
 def valvole():
     info("Add host valvolacucina\n")
     v1 = net.addDockerHost(
         "v1",
         dimage="valvola",
-        ip="10.0.0.3",
+        ip="10.0.0.2",
         docker_args={"environment": ["NAME=cucina"]},
     )
 
@@ -36,7 +38,7 @@ def valvole():
     v2 = net.addDockerHost(
         "v2",
         dimage="valvola",
-        ip="10.0.0.4",
+        ip="10.0.0.3",
         docker_args={"environment": ["NAME=camera"]},
     )
 
@@ -47,19 +49,20 @@ def valvole():
     v3 = net.addDockerHost(
         "v3",
         dimage="valvola",
-        ip="10.0.0.5",
+        ip="10.0.0.4",
         docker_args={"environment": ["NAME=bagno"]},
     )
 
     info("Add link\n")
     net.addLink(switch, v3, bw=10, delay="10ms")
 
+
 def umidificatori():
     info("Add host umidificatorecucina\n")
     u1 = net.addDockerHost(
         "u1",
         dimage="umidificatore",
-        ip="10.0.0.6",
+        ip="10.0.0.5",
         docker_args={"environment": ["NAME=cucina"]},
     )
 
@@ -70,7 +73,7 @@ def umidificatori():
     u2 = net.addDockerHost(
         "u2",
         dimage="umidificatore",
-        ip="10.0.0.7",
+        ip="10.0.0.6",
         docker_args={"environment": ["NAME=camera"]},
     )
 
@@ -81,19 +84,20 @@ def umidificatori():
     u3 = net.addDockerHost(
         "u3",
         dimage="umidificatore",
-        ip="10.0.0.8",
+        ip="10.0.0.7",
         docker_args={"environment": ["NAME=bagno"]},
     )
 
     info("Add link\n")
     net.addLink(switch, u3, bw=10, delay="10ms")
 
+
 def split():
     info("Add host splitcucina\n")
     s1 = net.addDockerHost(
         "s1",
         dimage="split",
-        ip="10.0.0.9",
+        ip="10.0.0.8",
         docker_args={"environment": ["NAME=cucina"]},
     )
 
@@ -104,30 +108,31 @@ def split():
     s2 = net.addDockerHost(
         "s2",
         dimage="split",
-        ip="10.0.0.11",
+        ip="10.0.0.9",
         docker_args={"environment": ["NAME=camera"]},
     )
 
     info("Add link\n")
     net.addLink(switch, s2, bw=10, delay="10ms")
-    
+
     info("Add host splitbagno\n")
     s3 = net.addDockerHost(
         "s3",
         dimage="split",
-        ip="10.0.0.12",
+        ip="10.0.0.10",
         docker_args={"environment": ["NAME=bagno"]},
     )
 
     info("Add link\n")
     net.addLink(switch, s3, bw=10, delay="10ms")
 
+
 def luci():
     info("Add host lucecucina\n")
     l1 = net.addDockerHost(
         "l1",
         dimage="luce",
-        ip="10.0.0.13",
+        ip="10.0.0.11",
         docker_args={"environment": ["NAME=cucina"]},
     )
 
@@ -138,7 +143,7 @@ def luci():
     l2 = net.addDockerHost(
         "l2",
         dimage="luce",
-        ip="10.0.0.14",
+        ip="10.0.0.12",
         docker_args={"environment": ["NAME=camera"]},
     )
 
@@ -149,19 +154,20 @@ def luci():
     l3 = net.addDockerHost(
         "l3",
         dimage="luce",
-        ip="10.0.0.15",
+        ip="10.0.0.13",
         docker_args={"environment": ["NAME=bagno"]},
     )
 
     info("Add link\n")
-    net.addLink(switch, l3, bw=10, delay="10ms")    
+    net.addLink(switch, l3, bw=10, delay="10ms")
+
 
 def tapparelle():
     info("Add host tapparellacucina\n")
     t1 = net.addDockerHost(
         "t1",
         dimage="tapparella",
-        ip="10.0.0.16",
+        ip="10.0.0.14",
         docker_args={"environment": ["NAME=cucina"]},
     )
 
@@ -172,7 +178,7 @@ def tapparelle():
     t2 = net.addDockerHost(
         "t2",
         dimage="tapparella",
-        ip="10.0.0.17",
+        ip="10.0.0.15",
         docker_args={"environment": ["NAME=camera"]},
     )
 
@@ -183,17 +189,40 @@ def tapparelle():
     t3 = net.addDockerHost(
         "t3",
         dimage="tapparella",
-        ip="10.0.0.18",
+        ip="10.0.0.16",
         docker_args={"environment": ["NAME=bagno"]},
     )
 
     info("Add link\n")
     net.addLink(switch, t3, bw=10, delay="10ms")
 
-def sonda():
-    info("Add host sonda\n")
+
+def sonde():
+    info("Add host sondacucina\n")
     so = net.addDockerHost(
-        "so",
+        "so1",
+        dimage="sonda",
+        ip="10.0.0.17",
+        docker_args={},
+    )
+
+    info("Add link\n")
+    net.addLink(switch, so, bw=10, delay="10ms")
+
+    info("Add host sondacamera\n")
+    so = net.addDockerHost(
+        "so2",
+        dimage="sonda",
+        ip="10.0.0.18",
+        docker_args={},
+    )
+
+    info("Add link\n")
+    net.addLink(switch, so, bw=10, delay="10ms")
+
+    info("Add host sondabagno\n")
+    so = net.addDockerHost(
+        "so3",
         dimage="sonda",
         ip="10.0.0.19",
         docker_args={},
@@ -201,6 +230,7 @@ def sonda():
 
     info("Add link\n")
     net.addLink(switch, so, bw=10, delay="10ms")
+
 
 def pulsantiera():
     info("Add host pulsantiera\n")
@@ -214,17 +244,19 @@ def pulsantiera():
     info("Add link\n")
     net.addLink(switch, p1, bw=10, delay="10ms")
 
+
 def applicazione():
     info("Add host applicazione\n")
     a1 = net.addDockerHost(
         "a1",
         dimage="applicazione",
-        ip="10.0.0.2",
+        ip="10.0.0.21",
         docker_args={},
     )
 
     info("Add link\n")
     net.addLink(switch, a1, bw=10, delay="10ms")
+
 
 if __name__ == "__main__":
     setLogLevel("info")
@@ -236,35 +268,24 @@ if __name__ == "__main__":
 
     info("Add switch s1\n")
     switch = net.addSwitch("s1")
-    
-    # broker
+
     broker()
-    
-    # valvole
+
     valvole()
-    
-    # umidificatori
+
     umidificatori()
-    
-    # split
+
     split()
 
-    # luci
     luci()
 
-    # tapparelle
     tapparelle()
 
-    # sonda
-    sonda()
+    sonde()
 
-    # pulsantiera
     pulsantiera()
 
-    # applicazione
-    applicazione()    
-    
-    # start
+    applicazione()
 
     info("Start\n")
     net.start()
